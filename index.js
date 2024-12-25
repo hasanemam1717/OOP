e36d34336de020e21c2854667e6f7d56ffd4017d
hasanemam1717 <hasanemam1717@gmail.com>
2024-12-25 16:45:55 +0600

    added class declrear

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ person.age = 15;
 
 // console.log(person);
 
-class Person {}
+class Person {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+}
 
-const P1 = new Person();
-
-P1.name = "Hasan Emam";
+const P1 = new Person("Hasan Emam", "hasanimam@web.com");
+const P2 = new Person("Hasan Emam1", "hasanimam1@web.com");
+const p3 = new Person("Sakib All hasan", "sakib@web.com");
 
 console.log(P1);
+console.log(P2);
+console.log(p3);
