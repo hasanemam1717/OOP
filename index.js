174b4b2ea30bcb2c877fcb35d1c1fe68decf3c49
hasanemam1717 <hasanemam1717@gmail.com>
2024-12-25 16:31:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const person = {};
+person.name = "Hasan Emam";
+person.age = 15;
+
+// console.log(person);
+
+class Person {}
+
+const P1 = new Person();
+
+P1.name = "Hasan Emam";
+
+console.log(P1);
