31d9dfdac5beba34ea7997abed24ccedf755a196
hasanemam1717 <hasanemam1717@gmail.com>
2024-12-28 09:05:41 +0600

    added Uniperson and Guardian class

---
+++ person/UniPerson.js
@@ -0,0 +1,35 @@
+const Person = require("./Person");
+
+const _department = Symbol("department");
+const _account = Symbol("account");
+
+class UniPerson extends Person {
+  constructor(id, name) {
+    super(id, name);
+    this[_department] = null;
+    this[_account] = null;
+  }
+  get department() {
+    return this[_department];
+  }
+  set department(value) {
+    this[_department] = value;
+  }
+
+  get account() {
+    return this[_account];
+  }
+  set account(value) {
+    this[_account] = value;
+  }
+
+  toString() {
+    // to call toString from Person class it will work to clg id and name
+    super.toString();
+    console.log(
+      ` Department= ${this[_department]} Account = ${this[_account]}`
+    );
+  }
+}
+
+module.exports = UniPerson;
