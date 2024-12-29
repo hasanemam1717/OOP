b049abbcbcbf8f92c71771e2c20b776b7ddf704d
hasanemam1717 <hasanemam1717@gmail.com>
2024-12-29 12:45:09 +0600

    added university department ,exam,subject

---
+++ university/Exam.js
@@ -0,0 +1,41 @@
+const _id = Symbol("id");
+const _name = Symbol("name");
+const _passMark = Symbol("passMark");
+const _subject = Symbol("subject");
+class Exam {
+  constructor(id, name, passMark, subject) {
+    this[_id] = id;
+    this[_name] = name;
+    this[_passMark] = passMark || 33;
+    this[_subject] = subject;
+  }
+  get id() {
+    return this[_id];
+  }
+  get name() {
+    return this[_name];
+  }
+  set name(value) {
+    this[_name] = value;
+  }
+  get passMark() {
+    return this[_passMark];
+  }
+  set passMark(value) {
+    this[_passMark] = value;
+  }
+  get subject() {
+    return this[_subject];
+  }
+  set subject(value) {
+    this[_subject] = value;
+  }
+  toString() {
+    return `
+      ID:${this[_id]}
+    Name:${this[_name]}
+    Subject:${this[_subject]}`;
+  }
+}
+
+module.exports = Exam;
